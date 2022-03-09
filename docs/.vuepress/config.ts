8f83a73e56af2a372e682cbf93e39627536e1aca
UwUssimo Robinson <uwussimo@icloud.com>
2022-03-09 13:35:10 +0500

    configs added

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,30 @@
+import { defineUserConfig } from 'vuepress'
+import type { DefaultThemeOptions } from 'vuepress'
+
+export default defineUserConfig<DefaultThemeOptions>({
+    lang: 'uz',
+    title: 'Osmon Wiki',
+    description: 'Osmon Dasturlash Tili Wikipediasi',
+    theme: '@vuepress/theme-default',
+    themeConfig: {
+        home: "/",
+        logo: 'https://raw.githubusercontent.com/osmon-lang/.github/main/ASSETS/Osmon%20Black.png',
+        logoDark: 'https://raw.githubusercontent.com/osmon-lang/.github/main/ASSETS/Osmon%20White.png',
+        repo: 'osmon-lang/osmon',
+        docsRepo: "osmon-lang/osmon-wiki",
+        docsBranch: "main",
+        editLink: true,
+        editLinkText: "Sahifaga o'zgartirish kiritish",
+        lastUpdated: true,
+        lastUpdatedText: "Oxirgi o'zgarish",
+        contributors: true,
+        contributorsText: "Mualliflar",
+        tip: "Maslahat",
+        warning: "Ogohlantiruv",
+        danger: "Ehtiyot bo'ling",
+        notFound: ["Afsuski ushbu sahifa mavjud emas...", "Bu yerda nimalar qilib yuribsiz?"],
+        backToHome: "Orqaga qaytish",
+        openInNewWindow: "yangi oynada ochish",
+        toggleDarkMode: "qiyofa rejimini o'zgartirish"
+    },
+})
